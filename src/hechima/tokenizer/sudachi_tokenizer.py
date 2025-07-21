6d4c5cdc3bfdd0655c0ba37aff8d20e30284ae3d
shimo8810 <shimo.8810@gmail.com>
2025-07-21 23:17:12 +0900

    feat: implement SudachiTokenizer class for Japanese text tokenization

---
+++ src/hechima/tokenizer/sudachi_tokenizer.py
@@ -0,0 +1,26 @@
+from collections.abc import Generator
+
+import sudachipy.dictionary
+import sudachipy.tokenizer
+
+from ..tokenizer import Tokenizer
+
+
+class SudachiTokenizer(Tokenizer):
+    def __init__(self):
+        """Initialize the Sudachi tokenizer."""
+        self._tokenizer = sudachipy.dictionary.Dictionary().create()
+        self._mode = sudachipy.tokenizer.Tokenizer.SplitMode.C
+
+    def tokenize(self, text: str) -> Generator[str]:
+        """Tokenize a given text into tokens.
+
+        Args:
+            text: The text to tokenize.
+
+        Returns:
+            A generator of tokens.
+        """
+        tokens = self._tokenizer.tokenize(text, self._mode)
+        for t in tokens:
+            yield t.surface()
