37ef7b0365b7856c45449c52c816cdfee37fdf91
shimo8810 <shimo.8810@gmail.com>
2025-07-21 23:16:59 +0900

    feat: add JanomeTokenizer class for Japanese text tokenization

---
+++ src/hechima/tokenizer/janome_tokenizer.py
@@ -0,0 +1,27 @@
+from collections.abc import Generator
+
+import janome.tokenizer
+
+from ..tokenizer import Tokenizer
+
+
+class JanomeTokenizer(Tokenizer):
+    def __init__(self):
+        """Initialize the Janome tokenizer."""
+        self._tokenizer = janome.tokenizer.Tokenizer()
+
+    def tokenize(self, text: str) -> Generator[str]:
+        """Tokenize a given text into tokens.
+
+        Args:
+            text: The text to tokenize.
+
+        Returns:
+            A generator of tokens.
+        """
+        tokens = self._tokenizer.tokenize(
+            text,
+            wakati=True,
+        )
+        for t in tokens:
+            yield str(t)
